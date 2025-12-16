9c6a256627fc2aac0c776d6bae8890b480e9e10e
AbhinavAlluvada <abhinav.alluvada@gmail.com>
2025-12-16 10:18:00 +0530

    ArrayList

--- array.java
+++ array.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class array {
-    public static void main(String[] args) {
+    void arr_int(){
         ArrayList <Integer> s1 = new ArrayList<Integer>();
         s1.add(10);
         s1.add(20);
@@ -11,6 +11,19 @@ public class array {
         System.out.println(s1.getFirst());
         System.out.println(s1.getLast());
         System.out.println(s1.isEmpty());
+    }
+    void arr_string(){
+        ArrayList<Object> s1 = new ArrayList<Object>();
+        s1.add("Hello");
+        s1.add("World");
+        System.out.println(s1);
+        s1.add(1,"Hi");
+        System.out.println(s1);
+    }
+    public static void main(String[] args) {
+        array obj = new array();
+        obj.arr_string();
+
 
     }
 }
